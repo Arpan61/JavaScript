9af6040d3d6bcb9153461b32f77b2cd30c80fd9c
Arpan Patel <arpanvpatel61@gmail.com>
2020-07-19 01:23:56 -0700

    Destructuring Example

---
+++ Destructuring.js
@@ -0,0 +1,15 @@
+//{ "name": "Rusty", "room": "kitchen", "weapon": "candlestick" }
+
+const { name, weapon, room } = { "name": "Rusty", "room": "kitchen", "weapon": "candlestick" }
+
+var [a, b] = [1, 2];
+console.log(a, b)
+
+var [a, , b] = [1, 2, 3]
+var [a, ...b] = [1, 2, 3]
+
+//swap without temp
+var a = 1, b = 2;
+[b, a] = [a, b]
+
+
